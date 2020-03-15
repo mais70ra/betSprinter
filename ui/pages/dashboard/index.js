cfacdd64f6d743b0842199765361f4e1c9320570
Dilyan Vasilev <dilyan.vasilev@Softwaregroup-bg.com>
2020-03-15 11:26:22 +0200

    feat: add bet buttons

--- ui/pages/dashboard/index.js
+++ ui/pages/dashboard/index.js
@@ -1,3 +1,7 @@
+var betObject = {
+
+};
+
 function logout() {
     reqWSC('user.logout', {})
     .then(r => {
@@ -8,4 +12,45 @@ function logout() {
     .catch(e => {
         popup.error(e.message);
     });
+}
+
+function betOnPlayer(btn, playerNumber) {
+    selectBetObject('btn-player', btn);
+    betObject.playerNumber = playerNumber;
+}
+
+function betAmount(btn, amount) {
+    selectBetObject('btn-amount', btn);
+    betObject.amount = amount;
+}
+
+function selectBetObject(className, btn) {
+    let previousSelectedButton = document.getElementsByClassName(className + '-selected')[0];
+    if (previousSelectedButton) {
+        previousSelectedButton.classList.remove(className + '-selected');
+    }
+    btn.classList.add(className + '-selected');
+}
+
+function finishBet() {
+    if (!betObject.playerNumber && !betObject.amount) {
+        popup.error('Please choose player and amount!');
+        return;
+    }
+    if (!betObject.playerNumber) {
+        popup.error('Please choose player!');
+        return;
+    }
+    if (!betObject.amount) {
+        popup.error('Please choose amount!');
+        return;
+    }
+}
+
+function disableAll() {
+
+}
+
+function enableAll() {
+
 }
